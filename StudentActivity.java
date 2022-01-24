9cf4635eda0e5f191d5d48e620737615efed6afa
mani-8 <98014465+mani-8@users.noreply.github.com>
2022-01-24 17:20:58 +0530

    Add files via upload

---
+++ StudentActivity.java
@@ -0,0 +1,37 @@
+package BasicProgram;
+
+import java.util.Scanner;
+
+class student{
+	float studentId;
+	String studentName;
+	float studentMark;
+
+
+
+public void setStudentData(float si, String sn, float sm) {
+	studentId=si;
+	studentName=sn;
+	studentMark=sm;
+	
+}
+}
+public class ClassDemo {
+
+public static void main(String [] args) {
+	float studentId,studentMark;
+	String studentName;
+	student st = new student();
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter your Id");
+	studentId=sc.nextFloat();
+	System.out.println("Enter your Name");
+	studentName=sc.next();
+	System.out.println("Enter your total mark");
+	studentMark=sc.nextFloat();
+	
+	st.setStudentData(studentId,studentName,studentMark);
+	System.out.println("Your ID: "+st.studentId +'\n'+"Your Name: "+st.studentName +'\n' +"Your Mark: "+st.studentMark);
+}
+
+}

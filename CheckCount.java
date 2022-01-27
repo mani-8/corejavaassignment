59949af1383223ea0ea3614d07214e52ce9a38d0
mani-8 <98014465+mani-8@users.noreply.github.com>
2022-01-27 18:55:37 +0530

    Add files via upload

---
+++ CheckCount.java
@@ -0,0 +1,18 @@
+package BasicProgram;
+
+import java.util.Scanner;
+
+public class CheckCount {
+	public static void main(String[] args) {
+		System.out.println("Enter the number");
+		Scanner sc = new Scanner(System.in);
+	    int number = sc.nextInt();
+	    sc.close();
+	    int count=0;
+			for(;number!=0;) {
+			    number/=10;
+			    count+=1;
+			}	
+			System.out.println("The count of number is : "+count);
+	}
+}

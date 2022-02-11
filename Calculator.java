028ab915ea3ece92d6370f6850ae2bf11e007953
mani-8 <98014465+mani-8@users.noreply.github.com>
2022-02-11 11:51:06 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+package banking;
+
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int i;
+		Scanner in=new Scanner(System.in);
+		do{
+			System.out.println("Calculator");
+			System.out.println("1.Addition \n2.Subtraction \n3.Multiplication \n4.Division \n5.Exit");
+			System.out.println("Enter your choise : ");
+			i=in.nextInt();
+			switch(i) {
+			case 1:
+				Calculations.add();;
+			    break;
+			case 2:
+				Calculations.calInfo();
+				Calculations.sub();
+			    break;
+			case 3:
+				Calculations.mul();
+			    break;
+			case 4:
+				Calculations.calInfo();
+				Calculations.div();
+			    break;
+			case 5:
+				break;
+			default:
+				System.out.println();
+				System.out.println("!!!! Enthe the Correct choise !!!!");
+				System.out.println();
+			}
+		}while(i!=5);
+		System.out.println("---Thank you---");
+		in.close();
+	}
+}
+© 2022 GitHub, Inc.
+Terms

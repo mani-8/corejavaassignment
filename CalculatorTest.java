a996b82679c9e8e1cff1a410089280834a3b23f3
mani-8 <98014465+mani-8@users.noreply.github.com>
2022-03-11 13:16:02 +0530

    Add files via upload

---
+++ CalculatorTest.java
@@ -0,0 +1,46 @@
+package junitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+	Calculator cal;
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("Before all");
+	}
+	@AfterAll
+	static void afterAll() {
+		System.out.println("After all");
+	}
+	@BeforeEach
+	void init() {
+		cal=new Calculator();
+		System.out.println("Before each test cases");
+	}
+	@AfterEach
+	void cleanUp(){
+		System.out.println("After each test cases");
+	}
+	
+	@Test
+	void addTest() {
+		assertEquals(20,cal.add(15,5));
+	}
+	@Test
+	void mulTest() {
+		assertEquals(10,cal.mul(5,2));
+	}
+	@Test
+	void maxTest() {
+		assertEquals(9,cal.max(new int[]{1,2,6,7,8,9}));
+	}
+	@Test
+	void errorTest() {
+		assertThrows(ArithmeticException.class,()->cal.div(10,0),"Exception");
+	}
+}

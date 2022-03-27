bd866a2d07b298b8c8743d63518dceef4fafd10c
mani-8 <98014465+mani-8@users.noreply.github.com>
2022-03-27 12:10:11 +0530

    Add files via upload

---
+++ CollectionExampleTest.java
@@ -0,0 +1,36 @@
+package junitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CollectionExampleTest {
+
+	CollectionExample cal=new CollectionExample();
+	
+	@Test
+	public void testAddNames()
+	{
+		cal.addNames("apurva");
+		cal.addNames("manikandan");
+		cal.addNames("Ajith");
+		cal.addNames("gayatri");
+		assertEquals(4,cal.lengthOfList(),"Adding 4 names in list");
+	}
+
+	@Test
+	public void testRemove()
+	{
+		cal.addNames("laxmi");
+		cal.addNames("sanjay");
+		cal.remove("sanjay");
+		assertEquals(1, cal.lengthOfList(),"Removing one student from list");
+	}
+	
+	@Test
+	public void testRemoveAll()
+	{
+		cal.removeAll();
+		assertEquals(0, cal.lengthOfList());
+	}
+}
